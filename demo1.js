07620d2dc14ac90806166665ffa4b9a6d12c148b
Intikhab Hussain <intikhabh@gmail.com>
2024-03-20 07:40:56 +0530

    branch rule added

--- demo1.js
+++ demo1.js
@@ -6,5 +6,7 @@ function addtwonumbers(){
 console.log("Adding New Feature")
 console.log("Another line added")
 console.log("Adding Feature B")
+console.log("New line")
+console.log("Branch rulues applied fully")
 
 }
